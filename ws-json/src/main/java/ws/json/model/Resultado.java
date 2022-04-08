fe7298c3c5e4195a3eac9df8b175333c16a8d979
csaccomanno <saccomanno@gmail.com>
2022-04-07 22:25:15 -0300

    Segundo

---
+++ ws-json/src/main/java/ws/json/model/Resultado.java
@@ -0,0 +1,5 @@
+package ws.json.model;
+
+public class Resultado {
+
+}
---
+++ ws-json/src/main/java/ws/json/model/Valor.java
@@ -0,0 +1,14 @@
+package ws.json.model;
+
+public class Valor {
+
+	private Object valor = null;;
+	
+	public Object getValor() {
+		return valor;
+	}
+
+	public void setValor(Object valor) {
+		this.valor = valor;
+	}
+}
