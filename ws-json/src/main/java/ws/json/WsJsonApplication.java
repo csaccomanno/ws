bf776e6d976fdbeb3ae1ab9a5dc3e951f311d2a0
csaccomanno <saccomanno@gmail.com>
2022-04-07 21:44:20 -0300

    Ws-Json Primer Commit 07/04/2022

---
+++ ws-json/src/main/java/ws/json/WsJsonApplication.java
@@ -0,0 +1,16 @@
+package ws.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class WsJsonApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WsJsonApplication.class, args);
+	}
+
+}
---
+++ ws-json/src/test/java/ws/json/WsJsonApplicationTest.java
@@ -0,0 +1,20 @@
+package ws.json;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class WsJsonApplicationTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
